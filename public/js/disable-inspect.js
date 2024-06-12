0c0bdba228062cab8ba76540136c97c60e531c42
denishraiyani <denish.raiyani96@gmail.com>
2024-06-12 17:41:02 +0530

    inactive contextmenu

---
+++ public/js/disable-inspect.js
@@ -0,0 +1,15 @@
+// Disable Right-click
+window.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
+
+// Disable Keyboard Shortcuts
+window.addEventListener("keydown", (e) => {
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey && e.shiftKey && (e.key == "I", e.key == "J", e.key == "C", e.key == "K")) ||
+    (e.ctrlKey && e.key === "U")
+  ) {
+    e.preventDefault();
+  }
+});
